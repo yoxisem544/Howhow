21c2a47eba1f07469ee5a6d45ecd0423826ad034
David Lin <yoxisem544@gmail.com>
2016-10-23 12:38:23 +0800

    remove unneeded code in collection view

--- Howhow/MainCollectionViewController.swift
+++ Howhow/MainCollectionViewController.swift
@@ -18,8 +18,7 @@ class MainCollectionViewController: UICollectionViewController {
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
-        // Register cell classes
-        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+		
 
         // Do any additional setup after loading the view.
     }
