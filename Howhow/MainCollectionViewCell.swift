a7eff112fdfeb52bb74f71a9e7a948d187ddd9ba
David Lin <yoxisem544@gmail.com>
2016-10-23 14:21:36 +0800

    add cell class

---
+++ Howhow/MainCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  MainCollectionViewCell.swift
+//  Howhow
+//
+//  Created by David on 2016/10/23.
+//  Copyright © 2016年 David. All rights reserved.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+	
+	override func awakeFromNib() {
+		super.awakeFromNib()
+		
+		// configuration
+	}
+	
+}
