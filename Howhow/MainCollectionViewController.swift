e054e07db07e064551eda5d62b9d5b17e1a8a1a8
David Lin <yoxisem544@gmail.com>
2016-10-23 20:19:19 +0800

    finish seuge

--- Howhow/MainCollectionViewController.swift
+++ Howhow/MainCollectionViewController.swift
@@ -29,15 +29,16 @@ class MainCollectionViewController: UICollectionViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    /*
+	
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using [segue destinationViewController].
         // Pass the selected object to the new view controller.
+		print(segue.identifier)
     }
-    */
+
 
     // MARK: UICollectionViewDataSource
 
