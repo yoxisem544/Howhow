c4b83b3104f55ba5729ba0c9e11ef8f256927695
David Lin <yoxisem544@gmail.com>
2016-10-23 12:47:45 +0800

    configure datasource

--- Howhow/MainCollectionViewController.swift
+++ Howhow/MainCollectionViewController.swift
@@ -42,13 +42,13 @@ class MainCollectionViewController: UICollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 0
+        return 10
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
